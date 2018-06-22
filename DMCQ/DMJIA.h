afdc80df4b180d0503bdb2511a509824eabc8d19
周聪 <614728425@qq.com>
2018-06-22 17:10:22 +0800

    push

---
+++ DMCQ/DMJIA.h
@@ -0,0 +1,14 @@
+//
+//  DMJIA.h
+//  DispatchingPoint
+//
+//  Created by 周聪 on 2018/6/20.
+//  Copyright © 2018年 DJSoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DMJIA : NSObject
+
+- (NSInteger)valuesJiATenSelf:(NSInteger) values;
+@end
---
+++ DMCQ/DMJIA.m
@@ -0,0 +1,15 @@
+//
+//  DMJIA.m
+//  DispatchingPoint
+//
+//  Created by 周聪 on 2018/6/20.
+//  Copyright © 2018年 DJSoft. All rights reserved.
+//
+
+#import "DMJIA.h"
+
+@implementation DMJIA
+- (NSInteger)valuesJiATenSelf:(NSInteger) values{
+    return values * 10;
+}
+@end
